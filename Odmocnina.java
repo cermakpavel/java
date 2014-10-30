9e33c3b1d3d3d749772c3e197189d686546953e2
Pavel Cermak <admin@cermakpavel.com>
2014-10-30 15:37:22 +0100

    Lekce 6

---
+++ Odmocnina.java
@@ -0,0 +1,17 @@
+public class Odmocnina {
+	public static void main(String[] args) {
+		double vstupniCislo, presnost = 0.01, odhad, predchoziOdhad;
+		vstupniCislo = Double.parseDouble(args[0]);
+		if (args.length >= 2) {
+			presnost = Double.parseDouble(args[1]);
+		}
+		odhad = vstupniCislo;
+		predchoziOdhad = Double.MAX_VALUE;
+		while (Math.abs(odhad-predchoziOdhad) > presnost) {
+			predchoziOdhad = odhad;
+			odhad = odhad + (vstupniCislo/odhad);
+			odhad /= 2;
+		}
+		System.out.printf("Odhad odmocniny z %.2f je %.2f.", vstupniCislo, odhad);
+	}
+}
